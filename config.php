b141eacad1016ec635e69629924be67c805500c9
shadowbot82 <42908347+shadowbot82@users.noreply.github.com>
2019-01-15 14:15:56 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+
+$uid="Facebook_338409416892935";
+$raw_uid="8af0bf2f-457f-4285-aa97-2b1bf3aa5d09";
+$user_name="Nani+Boled";
+$invite_code="F1Y5t1lzGx";
+$own_mac="00%3A27%3A15%3A76%3A02%3Ad1";
+$android_id="cab4f993b03e1788";
+$device_type="SPRD++B11";
+
+
+?>
